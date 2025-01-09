995843931b0e531399ca1ed1eee452338f27ca54
✨khanh🌟 <hoangkhanhvn179@gmail.com>
2025-01-09 11:12:32 +0700

    Update login page styles

--- index.js
+++ index.js
@@ -1,3 +1,4 @@
+// page: customer - order
 function updateSidebarForMobileView() {
   var logo = document.getElementById('logo');
   if (window.innerWidth <= 1280) {
@@ -46,4 +47,71 @@ document.getElementById('toggleSidebar').addEventListener('click', function () {
 
     toggleSidebar.style.removeProperty('position');
   }
+});
+
+// Page: index
+
+// event click login button
+document.getElementById('loginForm').addEventListener('submit', function (event) {
+  event.preventDefault();
+  const email = document.getElementById('email').value;
+  const password = document.getElementById('password').value;
+  const validEmail = 'user@gmail.com';
+  const validPassword = '123';
+
+  if (email === validEmail && password === validPassword) {
+    window.location.href = '/pages/customers.html';
+  } else {
+    document.getElementById('warning').style.display = 'block';
+  }
+});
+
+// event click show password
+document.getElementById('togglePassword').addEventListener('click', function () {
+  const passwordField = document.getElementById('password');
+  const toggleIcon = document.getElementById('toggleIcon');
+  if (passwordField.type === 'password') {
+    passwordField.type = 'text';
+    toggleIcon.classList.remove('bi-eye');
+    toggleIcon.classList.add('bi-eye-slash');
+  } else {
+    passwordField.type = 'password';
+    toggleIcon.classList.remove('bi-eye-slash');
+    toggleIcon.classList.add('bi-eye');
+  }
+});
+
+// event click language button
+function toggleOptions(id) {
+  const options = document.getElementById(id);
+  const allOptions = document.querySelectorAll('.options');
+
+  // Close all other option lists
+  allOptions.forEach((opt) => {
+    if (opt.id !== id) {
+      opt.style.display = 'none';
+    }
+  });
+
+  // Toggle current options
+  options.style.display = options.style.display === 'block' ? 'none' : 'block';
+}
+
+function selectOption(option, selectId) {
+  const select = document.querySelector(`#${selectId}`);
+  const selectBox = select.previousElementSibling;
+  const content = selectBox.querySelector('.select-content');
+  content.innerHTML =
+    option.querySelector('img').outerHTML + option.querySelector('span').textContent.substring(0, 3);
+  select.style.display = 'none';
+}
+
+// Close options when clicking outside
+document.addEventListener('click', function (e) {
+  const selects = document.querySelectorAll('.custom-select');
+  selects.forEach((select) => {
+    if (!select.contains(e.target)) {
+      select.querySelector('.options').style.display = 'none';
+    }
+  });
 });
